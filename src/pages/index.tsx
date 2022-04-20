443db926f94df6ff9a29640d4b32164fbbef1bed
John Li (Tet) <tet@tet.moe>
2022-04-20 00:34:53 -0700

    autoredirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,14 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useLayoutEffect } from "react";
 const speech =
   "Hi, I'm John! This year I'm trying to run for Vice President of United Computations. I have a few things to offer that will be useful for UC. For one, I am no stranger to CS. I have done many CS projects ranging from the Gunn Elimination app to an app for a better Schoology that even caught the attention of the school admins and district. I've had prior experience working well with upcoming UC president Saumya. Furthermore, being a rising senior, I will also have more time to dedicate to the club, as I don’t have any large, inflexible time commitments like GRT or lots of AP classes next year. Regardless of if I win or not, I wish you and UC all the best for the next school year!";
-const formurl = `https://docs.google.com/forms/d/e/${
-  Date.now() > 1650482400000
-    ? `1FAIpQLScUIfla_1vL6EplxlZykjjzDbMgIofQ2PcznmrDf0rMkIAScg`
-    : `1FAIpQLSfwTJjN6-1DRH4mpJoMLYJskqC7c1bqYbbhW4BW34mCT6RUrA`
-}/viewform?embedded=true&usp=pp_url&entry.1543329165=4th+pick&entry.333647976=2nd+pick&entry.907634041=1st+pick&entry.455228998=3rd+pick`;
+const formurl = `https://docs.google.com/forms/d/e/$1FAIpQLScUIfla_1vL6EplxlZykjjzDbMgIofQ2PcznmrDf0rMkIAScg/viewform?embedded=true&usp=pp_url&entry.1543329165=4th+pick&entry.333647976=2nd+pick&entry.907634041=1st+pick&entry.455228998=3rd+pick`;
 export default function Home() {
-  const iframeThing = useRef<HTMLIFrameElement>(null);
-  const [hue, setHue] = useState(0);
-  useEffect(() => {
-    const int = setInterval(() => {
-      setHue((h) => (h + 1) % 360);
-    }, 20);
-    return () => {
-      clearInterval(int);
-    };
-  }, []);
+  const router = useRouter();
+  useLayoutEffect(() => {
+    router.push(formurl);
+  });
   return (
     <>
       <Head>
@@ -41,62 +30,6 @@ export default function Home() {
         <meta property="og:site_name" content="Vote For Tet!" />
         <meta property="og:locale" content="en_US" />
       </Head>
-      <div className={`dark:bg-zinc-900 w-full h-full`}>
-        <div
-          className={`absolute top-0 left-0 w-full h-full dark:brightness-[0.2] opacity-30 dark:opacity-80 `}
-        >
-          <div
-            className={`w-full h-full`}
-            style={{
-              backgroundColor: `hsla(0,100%,50%,1)`,
-              backgroundImage: `
-          radial-gradient(at 40% 20%, hsla(28,100%,74%,1) 0px, transparent 50%),
-          radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%),
-          radial-gradient(at 0% 50%, hsla(355,100%,93%,1) 0px, transparent 50%),
-          radial-gradient(at 80% 50%, hsla(340,100%,76%,1) 0px, transparent 50%),
-          radial-gradient(at 0% 100%, hsla(22,100%,77%,1) 0px, transparent 50%),
-          radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%),
-          radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, transparent 50%)`,
-              filter: `hue-rotate(${hue}deg)`,
-            }}
-          />
-        </div>
-        <div
-          className={`w-full h-full `}
-          style={{
-            filter: `hue-rotate(${hue}deg)`,
-          }}
-        >
-          <iframe
-            // src="https://docs.google.com/forms/d/e/1FAIpQLScUIfla_1vL6EplxlZykjjzDbMgIofQ2PcznmrDf0rMkIAScg/viewform?embedded=true&usp=pp_url&entry.1543329165=4th+pick&entry.333647976=2nd+pick&entry.907634041=1st+pick&entry.455228998=3rd+pick"
-            src={formurl}
-            // src={'https://docs.google.com/forms/d/e/1FAIpQLSeiyQ9lhTdUZk0f9ST-0K9XxdAtP32nUhDF82GfL3bueRXyfQ/viewform'}
-
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; form-submit"
-            className={`w-full h-full dark:invert-[0.88] `}
-            ref={iframeThing}
-            id="gform"
-          >
-            <p>Lemonthink</p>
-          </iframe>
-        </div>
-      </div>
-      <Link href={formurl}>
-        <div
-          className={`bg-zinc-900 absolute bottom-0 left-0 z-20 p-2 m-4 rounded-lg text-white flex flex-row gap-8 pl-6 items-center cursor-pointer hover:bg-zinc-800`}
-        >
-          Form not loading? Click here to vote the old fashioned way!
-          <div
-            className={`font text-emerald-700 font-bold p-2 px-4 hover:bg-emerald-700/20 rounded-lg transition-all hover:text-emerald-300`}
-            style={{
-              filter: `hue-rotate(${hue}deg)`,
-            }}
-          >
-            VOTE
-          </div>
-        </div>
-      </Link>
     </>
   );
 }
